003830da51ab40610eb19f732b8d543e3378df47
doher <alexandr.pletezhov@gmail.com>
2020-02-20 10:03:47 +0300

    feat: add solution to simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,37 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position <= 0 || position > this.getLength()) {
+      this.chain = [];
+      throw Error;
+    }
+
+    if (!(typeof position === 'number')) {
+      this.chain = [];
+      throw Error;
+    }
+
+    this.getLength() && this.chain.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const arr = [...this.chain];
+    this.chain = [];
+    return arr.join('~~');
   }
 };
 
