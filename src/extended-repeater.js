a7fbdc6dca7caecedee3286c0156af4bbc8bc7a2
doher <alexandr.pletezhov@gmail.com>
2020-02-20 14:55:49 +0300

    feat: add solution to extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,42 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (str === null) {
+        str = 'null';
+    }
+
+    const repeatTimes = options['repeatTimes'];
+    const arr = new Array(repeatTimes);
+    let result = '';
+    let separator = '+';
+    let addition = '';
+    let additionRepeatTimes = 0;
+    let additionSeparator = '';
+    let additionStr = '';
+
+    if (options.hasOwnProperty('separator')) {
+        separator = options['separator'];
+    }
+
+    if (options.hasOwnProperty('addition')) {
+        addition = options['addition'];
+
+        if (addition === null) {
+            addition = 'null';
+        }
+    }
+
+    if (options.hasOwnProperty('additionRepeatTimes')) {
+        additionRepeatTimes = options['additionRepeatTimes'];
+    }
+
+    if (options.hasOwnProperty('additionSeparator')) {
+        additionSeparator = options['additionSeparator'];
+    }
+
+    const additionArr = new Array(additionRepeatTimes);
+
+    additionStr = additionArr.fill(addition).join(additionSeparator);
+    str += additionStr;
+    result = arr.fill(str).join(separator);
+
+    return result;
 };
-  
